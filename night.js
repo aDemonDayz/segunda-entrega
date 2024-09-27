d52f61b082dbef883fff8ec16034f9ca84f22bda
aDemonDayz <164443967+aDemonDayz@users.noreply.github.com>
2024-09-26 20:27:47 -0500

    Almost Finished Dark Mode

--- night.js
+++ night.js
@@ -1,3 +1,14 @@
 function modoOscuro(){
-    document.getElementById("body").classList.add("oscuro");    
+    document.getElementById("body").classList.add("oscuro");   
+    document.getElementById("nav").classList.add("dark"); 
+    document.getElementById("footer").classList.add("dark");
+    document.getElementById("enlace").classList.add("enlaceOscuro");
+
 }
+
+
+let cokies="Al aceptar ser un comegalletas, te comes nuestras cookies";
+alert(cokies);
+
+let tratamientoInfo="La informacion recopilada en esta pagina la vera nada mas el comegalletas";
+alert(tratamientoInfo);
